14db743f3edb0845863388254a45fc6ddf09b884
SOONY-PC\snowpipe <princeods@snowpipe.co.kr>
2022-05-25 22:24:31 +0900

    - 파트너 중복 포인트 체크 수정.

--- point_detection.py
+++ point_detection.py
@@ -1,5 +1,6 @@
 import os
 import glob
+from re import X
 from pandas import *
 from snowpipe import *
 from tqdm import tqdm
@@ -8,24 +9,50 @@ from tqdm import tqdm
 # -----------------------------------------------------------------------------------
 
 _pathSBLocation = "./space_bridge/"
-_pathSBPartner = "./partner_SB/"
+_pathSBPartner = "./partner_sb/"
+_pathResult = "./changeSB/" 
 
-_dfTargetSB = pandas.DataFrame(index=range(0,0),columns = ["PrimaryKey", "name", "latitude", "longitude"])
+_fStandardDistance = float(90)
+_dResultCount = 0
+
+_dfResult = pandas.DataFrame(index=range(0,0),columns = ["OriginKey", "OriginLatitude", "OriginLongitude",\
+     "PartnerName", "PartnerLatitude", "PartnerLongitude", 'distance'])
+#_dfPartner = pandas.DataFrame(index=range(0,0),columns = ["name", "latitude", "longitude"])
+
+_dfPartner = pandas.read_csv(_pathSBPartner + "1.csv")
 
 # -----------------------------------------------------------------------------------
 
-for _fileOriginSB in tqdm(glob.glob(_pathSBPartner+"*.csv")):
+CreateFolder(_pathResult)
 
-    for _fileTargetSB in tqdm(glob.glob(_pathSBLocation+"*.csv")):
+# -----------------------------------------------------------------------------------
 
-        _dfOriginSB = pandas.read_csv(_fileTargetSB)
+for _fileOriginSB in tqdm(glob.glob(_pathSBLocation+"*.csv")):
 
-        for i in tqdm(range(int(len(_dfOriginSB)))):
+    _dfOrigin= pandas.DataFrame(index=range(0,0))
+    _dfOrigin = pandas.read_csv(_fileOriginSB)
 
-            _dfOriginSB.loc[i,"latitude"]
-            _dfOriginSB.loc[i,"longitude"]
+    for _pointOriginCount in tqdm(range(0,len(_dfOrigin))):
 
-        _sFilename = os.path.basename(_fileOriginSB)
-        _sNameField = _sFilename[:2]
+        for i in range(0,len(_dfPartner)):
+            _fDiatance = CalcSb2SbDistance(_dfPartner.loc[i,'longitude'],_dfPartner.loc[i,'latitude'],\
+                _dfOrigin.loc[_pointOriginCount,'longitude'],_dfOrigin.loc[_pointOriginCount,'latitude'])
 
-    _ltTarget = []
+            if _fDiatance < _fStandardDistance:
+                _dfResult.loc[_dResultCount,'OriginKey'] = _dfOrigin.loc[_pointOriginCount,'PrimaryKey']
+                _dfResult.loc[_dResultCount,'OriginLatitude'] = _dfOrigin.loc[_pointOriginCount,"latitude"]
+                _dfResult.loc[_dResultCount,'OriginLongitude'] = _dfOrigin.loc[_pointOriginCount,'longitude']
+                _dfResult.loc[_dResultCount,'PartnerName'] = _dfPartner.loc[i,'name']
+                _dfResult.loc[_dResultCount,'PartnerLatitude'] = _dfPartner.loc[i,'latitude']
+                _dfResult.loc[_dResultCount,'PartnerLongitude'] = _dfPartner.loc[i,'longitude']
+                _dfResult.loc[_dResultCount,'distance'] = _fDiatance
+
+                _dResultCount += 1
+
+print(_dfResult)
+
+result = pandas.ExcelWriter(_pathResult + 'toChange.xlsx')
+
+_dfResult.to_excel(result, sheet_name='toChange')
+
+result.save()
