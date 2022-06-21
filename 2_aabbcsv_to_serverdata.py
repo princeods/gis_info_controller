cc26c8539156d96fb47228fb7f3894de51ea9fa2
SOONY-PC\snowpipe <princeods@snowpipe.co.kr>
2022-06-21 22:56:34 +0900

    - 이벤트 시간 / 건물 레벨에 가중치 적용.

--- 2_aabbcsv_to_serverdata.py
+++ 2_aabbcsv_to_serverdata.py
@@ -7,7 +7,8 @@ import pandas
 from tqdm import tqdm
 import datetime
 from enum import Enum
-
+import randy
+from snowpipe import *
 
 # -----------------------------------------------------------------------------------
 
@@ -26,7 +27,7 @@ _tStartTime = datetime.datetime.now()
 _dMaxNameLenth = 20
 _dSBLod = 65536     # 스페이스 브리지 영역 구분을 위한 기준 레벨
 _dSBLv3Gap = 500    # Lv. 3 스페이스 브리지 간격
-_dSBLv3Count = 0
+_dSBLv1Gap = 13     # Lv. 1 스페이스 브리지 간격
 
 _sNoName = "NO NAME"
 
@@ -35,12 +36,20 @@ _wsCubeSocket = "cubesocket"
 
 # create Structure Level list -----------------------------------------------------------------------------------
 
-_liLv1StructureNor = []
-_liLv2StructureNor = []
-_liLv3StructureNor = []
-_liLv1StructureLab = []
-_liLv2StructureLab = []
-_liLv3StructureLab = []
+_liLv1StructureNor = [1, 2, 3, 4, 5, 6]
+_liLv2StructureNor = [3, 4, 5, 6, 7, 8]
+_liLv3StructureNor = [5, 6, 7, 8, 9, 10]
+
+_liStructFactorNor = [3, 2, 2, 1, 1, 1]
+
+_liLv1StructureLab = [1, 1, 1]
+_liLv2StructureLab = [1, 2, 2]
+_liLv3StructureLab = [1, 2, 3]
+
+_liStructFactorLab = [3, 2, 1]
+
+_liEventTime =       [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 0, 1]
+_liEventTimeFactor = [1, 2, 2, 1,  1,  4,  4,  3,  2,  1,  2,  2,  3,  4,  5,  5,  5,  4, 3, 1]
 
 # for i in range(1,7):
 #     for j in reversed(range(1,pow(2,6-i)+1)):
@@ -51,15 +60,11 @@ _liLv3StructureLab = []
 #     for j in reversed(range(1,pow(2,6-i)+1)):
 #         _liLv3StructureNor.append(i+3)
 
-for i in range(1,7):
-    for j in reversed(range(1,int(pow(1.3,6-i)+1))):
-        _liLv1StructureNor.append(i)
-        _liLv2StructureNor.append(i+2)
-        _liLv3StructureNor.append(i+4)
-
-_liLv1StructureLab = [1]
-_liLv2StructureLab = [1, 1, 2]
-_liLv3StructureLab = [1, 1, 1, 1, 2, 2, 3]
+# for i in range(1,7):
+#     for j in reversed(range(1,int(pow(1.3,6-i)+1))):
+#         _liLv1StructureNor.append(i)
+#         _liLv2StructureNor.append(i+2)
+#         _liLv3StructureNor.append(i+4)
 
 # -----------------------------------------------------------------------------------
 
@@ -82,11 +87,11 @@ class SBLevel:
 
 # -----------------------------------------------------------------------------------
 
-def Dec2Hex(_dValue,_dDigit):
-    _sTemp = format(_dValue,"X")
-    for i in range(_dDigit - len(_sTemp)):
-        _sTemp = "0" + _sTemp
-    return _sTemp
+# def Dec2Hex(_dValue,_dDigit):
+#     _sTemp = format(_dValue,"X")
+#     for i in range(_dDigit - len(_sTemp)):
+#         _sTemp = "0" + _sTemp
+#     return _sTemp
 
 # -----------------------------------------------------------------------------------
 
@@ -147,16 +152,16 @@ for _fileOriginCsvfile in glob.glob(_pathLocation+"*.csv"):
         if _sTempName == "" or _dfOrigin.isnull().loc[i, "name"] or "," in _sTempName or \
             "\n" in _sTempName or "\t" in _sTempName or "\"" in _sTempName or  "\'" in _sTempName or \
                 "\r" in _sTempName:
-            _sSBLevel = Dec2Hex(1, 1)
+            _sSBLevel = 1
         else:
-            if (_dSBLv3Count / _dSBLv3Gap) == int(_dSBLv3Count / _dSBLv3Gap):
-                _sSBLevel = Dec2Hex(3, 1)
+            if (i / _dSBLv3Gap) == int(i / _dSBLv3Gap):
+                _sSBLevel = 3
+            elif (i / _dSBLv1Gap) == int(i / _dSBLv1Gap):
+                _sSBLevel = 1
             else:
-                _sSBLevel = Dec2Hex(2, 1)
+                _sSBLevel = 2
 
-        _dSBLv3Count += 1
-        _xPrimaryKey = _sSBLevel + _sCountryCode + _sFileNum + Dec2Hex(i, 4)
-        _dSBLevel = int( _sSBLevel, 16 )
+        _xPrimaryKey = Dec2Hex(_sSBLevel, 1) + _sCountryCode + _sFileNum + Dec2Hex(i, 4)
 
         # name dataframe append -----------------------------------------------------------------------------------
 
@@ -164,7 +169,7 @@ for _fileOriginCsvfile in glob.glob(_pathLocation+"*.csv"):
             
             _dfNameFile.loc[i, "PrimaryKey"] = int( _xPrimaryKey, 16 )
 
-            if _dSBLevel == 1:
+            if _sSBLevel == 1:
                 _dfNameFile.loc[i, _sNameField] = _sNoName
             else:
                 if len( _sTempName ) > _dMaxNameLenth:
@@ -185,42 +190,42 @@ for _fileOriginCsvfile in glob.glob(_pathLocation+"*.csv"):
 
         for j in range(0,6):
             if j == 0:    # 본부
-                if _dSBLevel == SBLevel.Lv1:
-                    _dTempHQBaseLevel = random.choice(_liLv1StructureNor)
-                elif _dSBLevel == SBLevel.Lv2:
-                    _dTempHQBaseLevel = random.choice(_liLv2StructureNor)
-                elif _dSBLevel == SBLevel.Lv3:
-                    _dTempHQBaseLevel = random.choice(_liLv3StructureNor)
+                if _sSBLevel == SBLevel.Lv1:
+                    _dTempHQBaseLevel = random.choices(_liLv1StructureNor, _liStructFactorNor)[0]
+                elif _sSBLevel == SBLevel.Lv2:
+                    _dTempHQBaseLevel = random.choices(_liLv2StructureNor, _liStructFactorNor)[0]
+                elif _sSBLevel == SBLevel.Lv3:
+                    _dTempHQBaseLevel = random.choices(_liLv3StructureNor, _liStructFactorNor)[0]
             elif j == 1:    # 요새
-                if _dSBLevel == SBLevel.Lv1:
-                    _dTempHQFortressLevel = random.choice(_liLv1StructureNor)
-                elif _dSBLevel == SBLevel.Lv2:
-                    _dTempHQFortressLevel = random.choice(_liLv2StructureNor)
-                elif _dSBLevel == SBLevel.Lv3:
-                    _dTempHQFortressLevel = random.choice(_liLv3StructureNor)
+                if _sSBLevel == SBLevel.Lv1:
+                    _dTempHQFortressLevel = random.choices(_liLv1StructureNor, _liStructFactorNor)[0]
+                elif _sSBLevel == SBLevel.Lv2:
+                    _dTempHQFortressLevel = random.choices(_liLv2StructureNor, _liStructFactorNor)[0]
+                elif _sSBLevel == SBLevel.Lv3:
+                    _dTempHQFortressLevel = random.choices(_liLv3StructureNor, _liStructFactorNor)[0]
             elif j == 2:    # 연구소
-                if _dSBLevel == SBLevel.Lv1:
-                    _dTempHQLabLevel = random.choice(_liLv1StructureLab)
-                elif _dSBLevel == SBLevel.Lv2:
-                    _dTempHQLabLevel = random.choice(_liLv2StructureLab)
-                elif _dSBLevel == SBLevel.Lv3:
-                    _dTempHQLabLevel = random.choice(_liLv3StructureLab)
+                if _sSBLevel == SBLevel.Lv1:
+                    _dTempHQLabLevel = random.choices(_liLv1StructureLab, _liStructFactorLab)[0]
+                elif _sSBLevel == SBLevel.Lv2:
+                    _dTempHQLabLevel = random.choices(_liLv2StructureLab, _liStructFactorLab)[0]
+                elif _sSBLevel == SBLevel.Lv3:
+                    _dTempHQLabLevel = random.choices(_liLv3StructureLab, _liStructFactorLab)[0]
             elif j == 3:    # 창고
-                if _dSBLevel == SBLevel.Lv1:
-                    _dTempHQWarehouseLevel = random.choice(_liLv1StructureNor)
-                elif _dSBLevel == SBLevel.Lv2:
-                    _dTempHQWarehouseLevel = random.choice(_liLv2StructureNor)
-                elif _dSBLevel == SBLevel.Lv3:
-                    _dTempHQWarehouseLevel = random.choice(_liLv3StructureNor)
+                if _sSBLevel == SBLevel.Lv1:
+                    _dTempHQWarehouseLevel = random.choices(_liLv1StructureNor, _liStructFactorNor)[0]
+                elif _sSBLevel == SBLevel.Lv2:
+                    _dTempHQWarehouseLevel = random.choices(_liLv2StructureNor, _liStructFactorNor)[0]
+                elif _sSBLevel == SBLevel.Lv3:
+                    _dTempHQWarehouseLevel = random.choices(_liLv3StructureNor, _liStructFactorNor)[0]
             elif j == 4:    # 전시관
                 _dTempHQExhibitionLevel = 1
             elif j == 5:    # 파츠 제작소
-                if _dSBLevel == SBLevel.Lv1:
-                    _dTempHQWorkshopLevel = random.choice(_liLv1StructureNor)
-                elif _dSBLevel == SBLevel.Lv2:
-                    _dTempHQWorkshopLevel = random.choice(_liLv2StructureNor)
-                elif _dSBLevel == SBLevel.Lv3:
-                    _dTempHQWorkshopLevel = random.choice(_liLv3StructureNor)
+                if _sSBLevel == SBLevel.Lv1:
+                    _dTempHQWorkshopLevel = random.choices(_liLv1StructureNor, _liStructFactorNor)[0]
+                elif _sSBLevel == SBLevel.Lv2:
+                    _dTempHQWorkshopLevel = random.choices(_liLv2StructureNor, _liStructFactorNor)[0]
+                elif _sSBLevel == SBLevel.Lv3:
+                    _dTempHQWorkshopLevel = random.choices(_liLv3StructureNor, _liStructFactorNor)[0]
 
 
         # spacebridge dataframe append ----------------------------------------------------------------------------
@@ -228,7 +233,7 @@ for _fileOriginCsvfile in glob.glob(_pathLocation+"*.csv"):
         if typeGeData.upper() == "A" or typeGeData.upper() == "S":
             
             _dfSBFile.loc[i, "PrimaryKey"] = int( _xPrimaryKey, 16 )
-            _dfSBFile.loc[i, "level"] = int( _sSBLevel, 16 )
+            _dfSBFile.loc[i, "level"] = _sSBLevel
             _dfSBFile.loc[i, "latitude"] = _dfOrigin.loc[i, "y"]
             _dfSBFile.loc[i, "longitude"] = _dfOrigin.loc[i, "x"]
             _dfSBFile.loc[i, "cubeSocketKey"] = int( _dfCubeSocket.loc[random.randint(0,_dSeedCubeSocketCount), "PrimaryKey"], 16 )
@@ -247,12 +252,15 @@ for _fileOriginCsvfile in glob.glob(_pathLocation+"*.csv"):
 
             _fRatioMainEvent = 0
 
-            if SBLevel.Lv1 == _dSBLevel:
+            if SBLevel.Lv1 == _sSBLevel:
                 _fRatioMainEvent = 0.05
-            elif SBLevel.Lv2 == _dSBLevel:
+                _dfSBFile.loc[i, "spacebridgepoint"] = 10
+            elif SBLevel.Lv2 == _sSBLevel:
                 _fRatioMainEvent = 0.1
-            elif SBLevel.Lv3 == _dSBLevel:
+                _dfSBFile.loc[i, "spacebridgepoint"] = 20
+            elif SBLevel.Lv3 == _sSBLevel:
                 _fRatioMainEvent = 1.0
+                _dfSBFile.loc[i, "spacebridgepoint"] = 30
 
             _dfSBFile.loc[i, "weekEvent00"] = "1111111"
 
@@ -260,11 +268,13 @@ for _fileOriginCsvfile in glob.glob(_pathLocation+"*.csv"):
                 _dfSBFile.loc[i, "timeEvent00"] = 24
                 _dfSBFile.loc[i, "minuteEvent00"] = random.randint(0,59)
             else:
-                _dfSBFile.loc[i, "timeEvent00"] = random.randint(6,24) % 24
+                # 1 차 이벤트 중 24 시간이 아닌 경우, 가중치를 적용해서 동접 많은 시간에 많이 나오게..                
+                _dfSBFile.loc[i, "timeEvent00"] = random.choices(_liEventTime, _liEventTimeFactor)[0]
                 _dfSBFile.loc[i, "minuteEvent00"] = random.randint(0,59)
 
             if random.random() <= 0.5:
                 # 50% 는 2 차 이벤트 발생
+                # 2 차 이벤트는 고르게 - 무작위로..
                 _dfSBFile.loc[i, "weekEvent01"] = "1111111"
                 _dfSBFile.loc[i, "timeEvent01"] = random.randint(6,25) % 24
                 _dfSBFile.loc[i, "minuteEvent01"] = random.randint(0,59)
@@ -272,13 +282,6 @@ for _fileOriginCsvfile in glob.glob(_pathLocation+"*.csv"):
                 _dfSBFile.loc[i, "weekEvent01"] = "0000000"
                 _dfSBFile.loc[i, "timeEvent01"] = 0
                 _dfSBFile.loc[i, "minuteEvent01"] = 0
-
-            if _dSBLevel == SBLevel.Lv3:
-                _dfSBFile.loc[i, "spacebridgepoint"] = 30
-            elif _dSBLevel == SBLevel.Lv2:
-                _dfSBFile.loc[i, "spacebridgepoint"] = 20
-            elif _dSBLevel == SBLevel.Lv1:
-                _dfSBFile.loc[i, "spacebridgepoint"] = 10
 
             _sBuildingInfo = Dec2Hex(_dTempHQWorkshopLevel, 1) + Dec2Hex(_dTempHQExhibitionLevel, 1) + \
                 Dec2Hex(_dTempHQWarehouseLevel, 1) + Dec2Hex(_dTempHQLabLevel, 1) + Dec2Hex(_dTempHQFortressLevel, 1) + Dec2Hex(_dTempHQBaseLevel, 1)
@@ -320,24 +323,24 @@ for _fileOriginCsvfile in glob.glob(_pathLocation+"*.csv"):
             _dDefenceTFCount = 0
             _dTempTFLevel = 0
             
-            if _dSBLevel == SBLevel.Lv3:
+            if _sSBLevel == SBLevel.Lv3:
                 _dDefenceTFCount = 3
                 _ltTF = _dfSeedTFA['StandardCode'].values.tolist() + _dfSeedTFS['StandardCode'].values.tolist()
-            elif _dSBLevel == SBLevel.Lv2:
+            elif _sSBLevel == SBLevel.Lv2:
                 _dDefenceTFCount = random.randint(2, 3)
                 _ltTF = _dfSeedTFB['StandardCode'].values.tolist() + _dfSeedTFA['StandardCode'].values.tolist() + _dfSeedTFS['StandardCode'].values.tolist()
-            elif _dSBLevel == SBLevel.Lv1:
+            elif _sSBLevel == SBLevel.Lv1:
                 _dDefenceTFCount = random.randint(1, 3)
                 _ltTF = _dfSeedTFC['StandardCode'].values.tolist() + _dfSeedTFB['StandardCode'].values.tolist()
 
             for k in range(1,4):
                 _dTempTFLevel = 0
                 
-                if _dSBLevel == SBLevel.Lv3:
+                if _sSBLevel == SBLevel.Lv3:
                     _dTempTFLevel = random.randint(13,15)
-                elif _dSBLevel == SBLevel.Lv2:
+                elif _sSBLevel == SBLevel.Lv2:
                     _dTempTFLevel = random.randint(2,9)
-                elif _dSBLevel == SBLevel.Lv1:
+                elif _sSBLevel == SBLevel.Lv1:
                     _dTempTFLevel = random.randint(1,4)
 
                 if k <= _dDefenceTFCount:
